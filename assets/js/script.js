80aa0c5254be6d62da259ef24c4cbe036a8fef35
Nitikapral <148422068+Nitikapral@users.noreply.github.com>
2023-12-06 16:27:37 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $(".accordion").click(function () {
+    $(this).find(".arrow").toggleClass("fa-chevron-down fa-chevron-up");
+  });
+});
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    {
+    }
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
